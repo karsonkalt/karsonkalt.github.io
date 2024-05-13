cf7a846a2ceb5ab680c0787ca228f157c6993553
Karson Kalt <karsonkalt@kkalt-MBP-M1.local>
2024-05-13 07:59:06 -0400

    update dev tools greeting

--- assets/js/devToolsGreeting.js
+++ assets/js/devToolsGreeting.js
@@ -17,7 +17,7 @@ console.log(`
         
 Hi there! 👋 I see you opened up the dev tools.
 
-This simple site is powered by Jekyll and GitHub pages and is really just a simple interface for me to jot down my thoughts in markdown.
+This site is powered by Jekyll and GitHub pages and is a simple interface for me to jot down my thoughts in markdown.
 
 If you're interested in connecting, please reach out to me via LinkedIn at:
 http://linkedin.com/in/kaltkarson
