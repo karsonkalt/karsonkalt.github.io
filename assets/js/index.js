ca95f93df450291f751b0a443e5449bd9afca832
Karson Kalt <kars.kalt@gmail.com>
2024-06-20 19:03:25 -0400

    update tabs, new styles with arrow

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,66 +23,6 @@ If you're interested in connecting, please reach out to me via LinkedIn at:
 http://linkedin.com/in/kaltkarson
 `);
 
-const TAB_QUERY_SELECTOR = "[role='tab']";
-
-const tabNodes = Array.from(document.querySelectorAll(TAB_QUERY_SELECTOR));
-
-tabNodes.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    history.replaceState(null, null, "#" + tab.id);
-    applyChanges(tab);
-  });
-});
-
-window.onhashchange = function () {
-  const tab = document.querySelector(location.hash);
-  if (tab) {
-    applyChanges(tab);
-  }
-};
-
-function applyChanges(tab) {
-  tabNodes.forEach((item) => {
-    const isSelected = item === tab;
-    item.setAttribute("aria-selected", isSelected.toString());
-    document
-      .getElementById(item.getAttribute("aria-controls"))
-      .setAttribute("aria-hidden", (!isSelected).toString());
-
-    let elements = document.querySelectorAll(".post-link");
-
-    elements.forEach((element) => {
-      element.style.opacity = "1";
-    });
-
-    ScrollReveal().reveal(elements, {
-      distance: "10%",
-      origin: "bottom",
-      reset: true,
-    });
-  });
-}
-
-if (location.hash) {
-  const tab = document.querySelector(location.hash);
-  if (tab) {
-    applyChanges(tab);
-  }
-}
-
-ScrollReveal().reveal(".post-link", {
-  distance: "10%",
-  origin: "bottom",
-  reset: true,
-});
-
-ScrollReveal().reveal("div.highlight", {
-  delay: 100,
-  distance: "5%",
-  reset: true,
-  origin: "bottom",
-});
-
 function copyToClipboard(text) {
   const textarea = document.createElement("textarea");
   textarea.value = text;
